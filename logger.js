98a5c83eb5891959139f097f026b84efb7177635
Yusuke-kun <japamnk@gmail.com>
2020-11-11 08:35:21 -0300

    end logger

---
+++ logger.js
@@ -0,0 +1,17 @@
+const evento = require('events')
+const fs = require('fs')
+const path = require('path')
+
+const emitter = new evento()
+
+emitter.on('log', (message) => {
+    fs.appendFile(path.join(__dirname , 'log.txt'), message, (err) => {
+        if(err) throw err = `Erro!! ... -> ${err}`
+    })
+})
+
+function log(message) {
+    emitter.emit('log', message)
+}
+
+module.exports = log
--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const os = require("os");
+const log = require('./logger')
 
 setInterval(() => {
     const { freemem, totalmem } = os;
@@ -15,7 +16,9 @@ setInterval(() => {
         
         console.clear();
         console.log(` === PC Stats === `);
-        console.table(stats);
+    console.table(stats);
+    
+    log(`${JSON.stringify(stats)}\n`)
 
 }, 1000);
 
